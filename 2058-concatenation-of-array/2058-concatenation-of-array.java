c6c603da63c8d47687909fb2ad87d62318267db2
Venigalla SHashank Chowdary <118808883+Shashankvenigalla@users.noreply.github.com>
2024-07-21 23:25:55 +0530

    Time: 1 ms (96.15%), Space: 42.6 MB (97.74%) - LeetHub

--- 2058-concatenation-of-array/2058-concatenation-of-array.java
+++ 2058-concatenation-of-array/2058-concatenation-of-array.java
@@ -3,6 +3,7 @@ class Solution {
         int[] num=new int[nums.length*2];
         System.arraycopy(nums,0,num,0,nums.length);
         System.arraycopy(nums , 0 ,num ,nums.length,nums.length);
+        System.gc();
         return num;
     }
 }
