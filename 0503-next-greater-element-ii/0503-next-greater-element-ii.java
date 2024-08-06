edf1e8d0d19ef7fc527d10866dd64f910f1232f8
Venigalla SHashank Chowdary <118808883+Shashankvenigalla@users.noreply.github.com>
2024-08-06 15:26:38 +0530

    Time: 1 ms (100%), Space: 46.3 MB (33.53%) - LeetHub

--- 0503-next-greater-element-ii/0503-next-greater-element-ii.java
+++ 0503-next-greater-element-ii/0503-next-greater-element-ii.java
@@ -1,18 +1,21 @@
 class Solution {
     public int[] nextGreaterElements(int[] nums) {
-        int a[]=new int[nums.length];
-        for(int i=0;i<nums.length;i++){
-            a[i]=-1;
-            int j=(i+1)%nums.length;
-            while(i!=j){
-                if(nums[j]>nums[i]){
-                    a[i]=nums[j];
-                    break;
-                }
-                else{
-                    j=(j+1)%nums.length;
-                }
-            }
-        }return a;
+        int n = nums.length;
+        int[] st = new int[2*n];
+        
+        int top = 0;
+        for(int i=n-1;i>=0;i--){
+            st[top++] = nums[i];
         }
+
+        int[] ans = new int[n];
+        for(int i=n-1;i>=0;i--){
+            while(top != 0 && st[top - 1] <= nums[i]){
+                top--;
+            }
+            ans[i] = top == 0 ? -1 : st[top - 1];
+            st[top++] = nums[i];
+        }
+        return ans;
+    }
 }
