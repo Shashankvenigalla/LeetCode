22fec6fd1968b11030aa28eecff815237d920a58
Venigalla SHashank Chowdary <118808883+Shashankvenigalla@users.noreply.github.com>
2024-08-06 15:25:57 +0530

    Time: 164 ms (5.02%), Space: 45.6 MB (95.78%) - LeetHub

--- 0503-next-greater-element-ii/0503-next-greater-element-ii.java
+++ 0503-next-greater-element-ii/0503-next-greater-element-ii.java
@@ -1,23 +1,18 @@
 class Solution {
     public int[] nextGreaterElements(int[] nums) {
-        int[] res = new int[nums.length];
-        int[] da = new int[2*nums.length];
-        for(int i = 0; i < nums.length; i++) {
-            da[i] = nums[i];
-        }
-        int k = 0;
-        for(int i = nums.length; i < da.length; i++) {
-            da[i] = nums[k];
-            k++;
-        }
-        for(int i = 0; i < nums.length; i++) {
-            res[i] = -1;
-            for(int j = i; j < da.length; j++) {
-                if(da[j] > da[i]) {
-                    res[i] = da[j];
+        int a[]=new int[nums.length];
+        for(int i=0;i<nums.length;i++){
+            a[i]=-1;
+            int j=(i+1)%nums.length;
+            while(i!=j){
+                if(nums[j]>nums[i]){
+                    a[i]=nums[j];
                     break;
                 }
+                else{
+                    j=(j+1)%nums.length;
+                }
             }
+        }return a;
         }
-        return res;}
 }
