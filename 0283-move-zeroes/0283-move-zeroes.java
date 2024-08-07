fab999f76024de1fb8296863d1e54da5bcd16f33
Venigalla SHashank Chowdary <118808883+Shashankvenigalla@users.noreply.github.com>
2024-08-07 15:24:41 +0530

    Time: 2 ms (83%), Space: 45.7 MB (77.21%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.java
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -3,12 +3,14 @@ class Solution {
         int i =0, j=0, n=nums.length;
         for (i=0; i<n; i++) {
             if (nums[i]!=0) {
+                int temp = nums[j];
                 nums[j] = nums[i];
+                nums[i]=temp;
                 j++;
             }
         }
-         for (;j<n;j++) {
-           nums[j] = 0;
-            }
+        // for (;j<n;j++) {
+        //     nums[j] = 0;
+        //     }
     }
 }
