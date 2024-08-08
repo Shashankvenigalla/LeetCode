8f4a48c7ae0127e3c9fe6b85d50fbde49c6297ab
Venigalla SHashank Chowdary <118808883+Shashankvenigalla@users.noreply.github.com>
2024-08-08 12:29:17 +0530

    Time: 0 ms (100%), Space: 46.1 MB (30.42%) - LeetHub

--- 0136-single-number/0136-single-number.java
+++ 0136-single-number/0136-single-number.java
@@ -1,11 +1,12 @@
 class Solution {
     public int singleNumber(int[] nums) {
-        int xor=0;
-        for(int i=0;i<nums.length;i++){
-            xor^=nums[i] ;
-            System.out.print(xor);
-        }
-        return xor;
-        
+        int x=nums[0];
+        return find(nums,1,2,x);
+    }
+    public int find(int[] arr,int i,int j,int x)
+    {
+        if(arr.length==i || arr.length==j) return x;
+        x=x^arr[i]^arr[j];
+        return find(arr,j+1,j+2,x);
     }
 }
