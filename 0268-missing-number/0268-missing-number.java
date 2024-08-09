8b815eec0a85458bc321129bc6e3cfac0f43c139
Venigalla SHashank Chowdary <118808883+Shashankvenigalla@users.noreply.github.com>
2024-08-09 09:55:17 +0530

    Time: 0 ms (100%), Space: 45 MB (80.51%) - LeetHub

--- 0268-missing-number/0268-missing-number.java
+++ 0268-missing-number/0268-missing-number.java
@@ -1,36 +1,9 @@
 class Solution {
-     public static int missingNumber(int[] arr){
-        int ans = arr.length;
-        cyclicSort(arr);
-        for(int i = 0; i < arr.length; i++){
-            if(arr[i] != i) {
-                ans = i;
-                break;
-            }
-        }
-        return ans;
-    }
-
-    public static void cyclicSort(int[] arr){
-        int n = arr.length;
-        for(int i = 0; i < n;){
-            if(arr[i] == n){
-                i++;
-            }
-            else{
-                if(arr[i] == i){
-                    i++;
-                }
-                else{
-                    swap(arr, arr[i], i);
-                }
-            }
-        }
-    }
-
-    public static void swap(int[] arr, int expIndex, int indexOg){ 
-        int temp = arr[indexOg];
-        arr[indexOg] = arr[expIndex];
-        arr[expIndex] = temp;
+    public int missingNumber(int[] nums) {
+        int sum=0;
+        int n=nums.length;
+        for(int i=0;i<n;i++) sum=sum+nums[i];
+        int diff=n*(n+1)/2;
+        return (diff-sum);
     }
 }
