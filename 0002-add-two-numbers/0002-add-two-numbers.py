1e449d6572fe3a9884f3e9d1f73c75a342cc3c1f
Venigalla SHashank Chowdary <118808883+Shashankvenigalla@users.noreply.github.com>
2024-09-08 14:39:05 +0530

    Time: 53 ms (17.71%), Space: 11.6 MB (71.11%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.py
+++ 0002-add-two-numbers/0002-add-two-numbers.py
@@ -1,24 +1,30 @@
 class Solution(object):
     def addTwoNumbers(self, l1, l2):
-        dummy = ListNode(0)
-        current = dummy
+        """
+        :type l1: ListNode
+        :type l2: ListNode
+        :rtype: ListNode
+        """
+        ptr1 = l1
+        ptr2 = l2
+        
+        res_head = ListNode()
+        res_ptr = res_head
         carry = 0
-        
-        while l1 or l2 or carry:
-            val1 = l1.val if l1 else 0
-            val2 = l2.val if l2 else 0
+        while ptr1 or ptr2:
+            val1 = ptr1.val if ptr1 else 0
+            val2 = ptr2.val if ptr2 else 0
+            add = val1 + val2 + carry
+            r = add%10
+            carry = add // 10
+            new_res_node = ListNode(r)
+            res_ptr.next = new_res_node
+            res_ptr = new_res_node
+
             
-            total = val1 + val2 + carry
-            carry = total // 10
-            new_digit = total % 10
-            
-            current.next = ListNode(new_digit)
-            current = current.next
-            
-            if l1:
-                l1 = l1.next
-            if l2:
-                l2 = l2.next
+            ptr1 = ptr1.next if ptr1 else None
+            ptr2 = ptr2.next if ptr2 else None
         
-        return dummy.next
-        
+        if carry:
+            res_ptr.next = ListNode(carry)
+        return res_head.next
